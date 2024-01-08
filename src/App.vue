273cf142bc866277eb02a53f82dc6a42898c63d8
stappost <emanuele.scarpantoni@gmail.com>
2024-01-08 18:58:22 +0100

    setup components, App.vue and scss file

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+import AppFooter from './components/AppFooter.vue'
+
+export default {
+    components: {
+        AppHeader,
+        AppMain,
+        AppFooter
+    }
+}
+</script>
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+<style lang="scss">
+@use './styles/generals.scss' as *;
+@use './styles/partials/variabiles' as *;
+</style>
