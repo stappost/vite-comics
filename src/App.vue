78dc40751e2b9c29f1299280f39043c880cb8f00
stappost <emanuele.scarpantoni@gmail.com>
2024-01-08 19:49:32 +0100

    add first part of main

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <script>
 import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
 
 export default {
     components: {
         AppHeader,
+        AppMain,
     }
 }
 </script>
 <template lang="">
     <div>
         <AppHeader />
-
+        <AppMain />
     </div>
 </template>
 <style lang="scss">
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,13 +1,30 @@
 <script>
 export default {
-
+    name: "AppMain",
 }
 </script>
 <template lang="">
-    <div>
-        
-    </div>
+    <main>
+        <div class="bg_black">
+            <div class="container">
+                <h3 class="text-white">
+                 --> Content goes here <--
+                </h3>
+            </div>
+        </div>
+    </main>
 </template>
-<style lang="">
-    
+<style lang="scss" scoped>
+main {
+    margin: 20px 0;
+
+    .bg_black {
+        background-color: #000;
+        padding: 50px;
+
+        .text-white {
+            color: #fff;
+        }
+    }
+}
 </style>
